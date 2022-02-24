3e568f57ce641ab30395e228576d09b64dee58ad
UE4SS <wildbrim@hotmail.com>
2022-02-24 08:04:58 +0100

    UnrealVirtuals: Added a safety check that prevents any attempt to call a virtual function if we don't have an offset in the vtable for it.

--- include/Unreal/VersionedContainer/UnrealVirtualImpl/UnrealVirtualBaseVC.hpp
+++ include/Unreal/VersionedContainer/UnrealVirtualImpl/UnrealVirtualBaseVC.hpp
@@ -5,6 +5,10 @@
 #define ARGS(...)  __VA_ARGS__
 
 #define IMPLEMENT_UNREAL_VIRTUAL_WRAPPER_NO_PARAMS(class_name, function_name, return_type) \
+if (VTableOffsets::function_name == 0)                                                         \
+{                                                                                              \
+    throw std::runtime_error{"Virtual " #class_name "::" #function_name " is unavailable, possibly unsupported in engine version"}; \
+}                                                                                           \
 std::byte* vtable = std::bit_cast<std::byte*>(*std::bit_cast<std::byte**>(this)); \
 RC::Function<return_type(const class_name*)> func = *std::bit_cast<void**>(vtable + VTableOffsets::function_name); \
 if (!func.is_ready()) \
@@ -14,6 +18,10 @@ throw std::runtime_error{"Function '" #function_name "' not available"}; \
 return func(this);
 
 #define IMPLEMENT_UNREAL_VIRTUAL_WRAPPER(class_name, function_name, return_type, params, args) \
+if (VTableOffsets::function_name == 0)                                                         \
+{                                                                                              \
+    throw std::runtime_error{"Virtual " #class_name "::" #function_name " is unavailable, possibly unsupported in engine version"}; \
+}\
 std::byte* vtable = std::bit_cast<std::byte*>(*std::bit_cast<std::byte**>(this)); \
 RC::Function<return_type(const class_name*, params)> func = *std::bit_cast<void**>(vtable + VTableOffsets::function_name); \
 if (!func.is_ready()) \
